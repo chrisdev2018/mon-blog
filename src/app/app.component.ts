2b28ba11eda82a1d3a3f7b8a4a4f89fde88d908f
Christian FOMEKONG <christian.fomekong@corpecca.africa>
2019-07-05 13:27:08 +0100

    [ADD] Ajoute le tableau de posts dans le AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,41 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Mon premier projet Angular';
 
- 
+  post: {
+    title: string,
+    content: string,
+    like: number,
+    dislike: number,
+    create_at: number
+
+  };
+
+  content = 'Le code complet et fonctionnel doit être déposé dans un dépôt\
+   Git en ligne que les validateurs doivent pouvoir cloner. ';
+
+  allPost = [
+    {
+      title: 'Mon premier post',
+      content: this.content,
+      like: 0,
+      dislike: 0,
+      create_at: new Date()
+    },
+    {
+      title: 'Mon Deuxième post',
+      content: this.content,
+      like: 0,
+      dislike: 0,
+      create_at: new Date()
+    },
+    {
+      title: 'Encore un post post',
+      content: this.content,
+      like: 0,
+      dislike: 0,
+      create_at: new Date()
+    }
+  ];
+
+
 }
